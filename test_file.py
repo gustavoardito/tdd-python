aa8cd1f99d09d8b988de342bcc372083e22592fd
Gustavo Ardito <gardito@eventbrite.com>
2018-09-09 19:59:46 -0300

    lesson 11 - using fixtures part 1

---
+++ test_file.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.fixture()
+def setup():
+    print("\nSetup")
+
+
+def test1(setup):
+    print("Executing test1!")
+    assert True
+
+
+@pytest.mark.usefixtures("setup")
+def test2():
+    print("Executing test2!")
+    assert True

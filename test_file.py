08dd192634ff7cfae6b3782f958a9f14cd7e0046
Gustavo Ardito <gardito@eventbrite.com>
2018-09-09 23:44:44 -0300

    lesson 11- using fixture part 2

--- test_file.py
+++ test_file.py
@@ -1,17 +1,16 @@
 import pytest
 
 
-@pytest.fixture()
+@pytest.fixture(autouse=True)
 def setup():
     print("\nSetup")
 
 
-def test1(setup):
+def test1():
     print("Executing test1!")
     assert True
 
 
-@pytest.mark.usefixtures("setup")
 def test2():
     print("Executing test2!")
     assert True

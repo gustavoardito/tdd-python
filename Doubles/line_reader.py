0a65466172d05ef55ceaa2b9315a38c5cffbadc5
Gustavo Ardito <gardito@eventbrite.com>
2019-12-01 20:54:05 -0300

    line reader test example

--- Doubles/line_reader.py
+++ Doubles/line_reader.py
@@ -1,4 +1,10 @@
+import os
+
+
 def read_from_file(file_name):
+    if not os.path.exists(file_name):
+        raise Exception('Bad File')
+
     in_file = open(file_name, 'r')
     line = in_file.readline()
     return line
